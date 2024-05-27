6b5ce5e6bf3042ea242757b9e2a57f2d0973a5b3
alexandrataranets <142220751+alexandrataranets@users.noreply.github.com>
2024-05-27 17:28:16 +0200

    apdate ImageModal.jsx

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,35 +1,37 @@
 import Modal from 'react-modal';
 
-
 Modal.setAppElement('#root');
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    border: 'none',
-    padding: '0',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    cursor: 'zoom-out',
-  },
-  overlay: {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: 'rgba(0, 0, 0, 0.75)',
-  },
-};
-
-export default function ImageModal({ isOpen, onClose, imageUrl }) {
+export default function ImageModal({ image, onClose }) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onClose} style={customStyles} contentLabel="Image Modal">
+    <Modal
+      isOpen={!!image}
+      onRequestClose={onClose}
+      contentLabel="Image Modal"
+      style={{
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          border: 'none',
+          padding: '0',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          cursor: 'zoom-out',
+        },
+        overlay: {
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: 'rgba(0, 0, 0, 0.75)',
+        },
+      }}
+    >
       <div onClick={onClose}>
-        <img src={imageUrl} alt="Large" />
+        <img src={image.urls.regular} alt={image.alt_description} />
       </div>
     </Modal>
   );
